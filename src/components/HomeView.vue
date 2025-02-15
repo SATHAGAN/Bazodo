9b8fcf31ae5c3f366486f6d671ac311c8905675b
sathagan <sathagan2003@gmail.com>
2025-02-15 22:35:52 +0530

    new change

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -23,10 +23,11 @@
     </div>
     <div class="shopcol">
       <div class="gridtry">
-        <h1>Say 
-            <span class="gc">Goodbye</span> to
-           <span class="rc">High Shipping</span>
-             Costs!</h1>
+        <h1>
+          Say <span class="gc">Goodbye</span> to
+          <span class="rc">High Shipping</span>
+          Costs!
+        </h1>
         <p>
           Tired of hefty shipping costs? At Bazodo, we've got you covered. With
           our exclusive shipping discounts, you can shop worry-free and save big
@@ -35,14 +36,14 @@
 
         <a href=""> <button>Shop Collection</button></a>
         <a href="">Check Offers</a>
-       <!-- <img
+        <!-- <img
             src="https://0464nbun00.ufs.sh/f/ulJ46yIdWMrJG9Xb7LXh91OtTjc2bKhUBq5GnHVdxE6SzDPW"
             alt="Grow Bag"
           /> -->
       </div>
-      
-        <div class="gridtwo">
-          <!-- <img
+
+      <div class="gridtwo">
+        <!-- <img
             src="https://0464nbun00.ufs.sh/f/ulJ46yIdWMrJG9Xb7LXh91OtTjc2bKhUBq5GnHVdxE6SzDPW"
             alt="Grow Bag"
           />
@@ -56,44 +57,42 @@
             src="https://0464nbun00.ufs.sh/f/ulJ46yIdWMrJG9Xb7LXh91OtTjc2bKhUBq5GnHVdxE6SzDPW"
             alt="Grow Bag"
           />-->
-          <!-- <img
+        <!-- <img
             src="https://0464nbun00.ufs.sh/f/ulJ46yIdWMrJG9Xb7LXh91OtTjc2bKhUBq5GnHVdxE6SzDPW"
             alt="Grow Bag"
           /> -->
-        </div>
-      </div>
-      <div>
-        <FreaturedProduct/>
-      </div>
-      <div>
-        <card-pro/>
-      </div>
-      <div>
-        <FreaturedProduct/>
-      </div>
-      <div>
-        <plat-form/>
-      </div>
-       <div>
-        <FreaturedProduct/>
-      </div>
-      <div>
-        <re-view/>
-      </div>
-      <div>
-        <about-end/>
       </div>
     </div>
-  
+    <div>
+      <FreaturedProduct />
+    </div>
+    <div>
+      <card-pro />
+    </div>
+    <div>
+      <FreaturedProduct />
+    </div>
+    <div>
+      <plat-form />
+    </div>
+    <div>
+      <FreaturedProduct />
+    </div>
+    <div>
+      <re-view />
+    </div>
+    <div>
+      <about-end />
+    </div>
+  </div>
 </template>
 
-
 <script lang="ts">
-import AboutEnd from './AboutEnd.vue'
-import ReView from './ReView.vue'
-import PlatForm from './PlatForms.vue'
-import CardPro from './CardPro.vue'
-import FreaturedProduct from "./FreaturedProduct.vue"
+import AboutEnd from "./AboutEnd.vue";
+import ReView from "./ReView.vue";
+import PlatForm from "./PlatForms.vue";
+import CardPro from "./CardPro.vue";
+import FreaturedProduct from "./FreaturedProduct.vue";
 import CollecTion from "./CollecTion.vue";
 export default {
   name: "App",
@@ -103,7 +102,7 @@ export default {
     CardPro,
     PlatForm,
     ReView,
-    AboutEnd
+    AboutEnd,
   },
   methods: {
     // prevSlide() {
@@ -114,125 +113,12 @@ export default {
     //   const scroll = document.querySelector('.scroll');
     //   scroll.style.transform = `translateX(-${(currentSlide + 1) * 100}%)`;
     // },
-    color:{
-        Goodbye:'green',
-    }
+    color: {
+      Goodbye: "green",
+    },
   },
 };
 </script>
-<style>
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-}
+<style src="@/assets/Styles/demo.css">
 
-.slider {
-  /* padding-top: 115px; */
-  min-width: 50px;
-
-  position: relative;
-  margin: 20px;
-  overflow: hidden;
-}
-
-.scroll {
-  display: flex;
-  width: 100%;
-  animation: slide 18s infinite;
-}
-
-.scroll img {
-  width: 100%;
-  height: 100%;
-}
-
-@keyframes slide {
-  0% {
-    transform: translateX(0%);
-  }
-  50% {
-    transform: translateX(-100%);
-  }
-  75% {
-    transform: translateX(-200);
-  }
-  100% {
-    transform: translateX(0%);
-  }
-}
-.prev,
-.next {
-  position: absolute;
-  top: 30%;
-  transform: translateY(-50%);
-  background-color: rgba(0, 0, 0, 0.5);
-  color: white;
-  border: none;
-  padding: 10px;
-  cursor: pointer;
-}
-
-.prev {
-  left: 20px;
-}
-
-.next {
-  right: 20px;
-}
-.coll {
-  justify-items: center;
-}
-.shopcol {
-  display:flex;
-  padding: 30px;
-  gap: 20px;
-}
-.gridtwo {
-  grid-auto-columns: 200px;
-  grid-template-areas: "a a";
-  padding: 10px;
-}
-.gridtwo img {
-  height: auto;
-  margin: 0 auto;
-}
-.gridtry h1{
-    text-align: initial;
-    font-size: 60px;
-}
-.gridtry p{
-    display: flex;
-    text-align: start;
-    font-size: 1.2rem;
-    line-height: 1.75 rem;
-    font-size: 30px;
-}
-.gridtry button{
-    padding: 20px;
-    margin: 30px;
-    color: white ;
-    background: green;
-    border-radius: 5px;
-    font-size: 20px;
-}
-.gridtry a{
-    text-decoration-line: none;
-    color: green;
-    padding: 10px;
-    margin: 30px;
-    font-size: 20px;
-
-}
-.gridtry img{
-    
-    justify-items: flex-end;
-    align-items: flex-end;
-}
-.gc{
-    color: green;
-}
-.rc{
-    color: red;
-}
 </style>
